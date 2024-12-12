bd9981b9e5204b8cabab18ce4f6383010b3ad792
horiyu <horiyu.dev@gmail.com>
2024-12-13 04:38:45 +0900

    ブランドフォントとテキストスタイルを定義するクラスを追加

---
+++ lib/presentation/theme/fonts.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+import 'package:my_web_app/presentation/theme/sizes.dart';
+
+class BrandFont {
+  static const general = 'NotoSansJP';
+}
+
+class BrandText {
+  static const titleL = TextStyle(
+    fontSize: RawSize.p28,
+    fontFamily: BrandFont.general,
+    fontWeight: FontWeight.bold,
+  );
+  static const titleM = TextStyle(
+    fontSize: RawSize.p24,
+    fontFamily: BrandFont.general,
+    fontWeight: FontWeight.bold,
+  );
+  static const titleS = TextStyle(
+    fontSize: RawSize.p20,
+    fontFamily: BrandFont.general,
+  );
+  static const bodyL = TextStyle(
+    fontSize: RawSize.p18,
+    fontFamily: BrandFont.general,
+  );
+  static const bodyM = TextStyle(
+    fontSize: RawSize.p16,
+    fontFamily: BrandFont.general,
+  );
+  static const bodyS = TextStyle(
+    fontSize: RawSize.p16,
+    fontFamily: BrandFont.general,
+  );
+}
