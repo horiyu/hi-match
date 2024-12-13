e709e26944e6cee5ca3dad379404c60cca7a692f
horiyu <horiyu.dev@gmail.com>
2024-12-13 15:22:28 +0900

    HimaListPageを追加し、基本的なUIプレースホルダーを実装

---
+++ lib/presentation/pages/hima_list.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class HimaListPage extends StatefulWidget {
+  const HimaListPage({super.key});
+
+  @override
+  State<HimaListPage> createState() => _HimaListPageState();
+}
+
+class _HimaListPageState extends State<HimaListPage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
