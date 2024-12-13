a6645359a32c49e98045abe3491429e1ed192198
horiyu <horiyu.dev@gmail.com>
2024-12-13 17:07:12 +0900

    UserIconウィジェットを追加し、ユーザー画像とステータス表示機能を実装

---
+++ lib/presentation/widgets/user_icon.dart
@@ -0,0 +1,61 @@
+import 'package:flutter/material.dart';
+
+class UserIcon extends StatelessWidget {
+  final String? imageUrl;
+  final double size;
+  final bool? isDisplayedStatus;
+  final bool? isStatus;
+  final void Function()? onTap;
+
+  const UserIcon({
+    super.key,
+    this.imageUrl,
+    this.isDisplayedStatus,
+    this.isStatus,
+    this.onTap,
+    required this.size,
+  }) : assert(
+          isDisplayedStatus == null ||
+              isDisplayedStatus == false ||
+              isStatus != null,
+          'isStatus must be provided when isDisplayedStatus is true',
+        );
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: onTap,
+      child: Stack(
+        children: [
+          Container(
+            width: size,
+            height: size,
+            decoration: BoxDecoration(
+              shape: BoxShape.circle,
+              image: DecorationImage(
+                image: imageUrl != null && imageUrl!.isNotEmpty
+                    ? NetworkImage(imageUrl!)
+                    : const AssetImage('assets/images/default_user_icon.png')
+                        as ImageProvider,
+                fit: BoxFit.cover,
+              ),
+            ),
+          ),
+          if (isDisplayedStatus ?? false)
+            Positioned(
+              right: 0,
+              bottom: 0,
+              child: Container(
+                width: size * 0.2,
+                height: size * 0.2,
+                decoration: BoxDecoration(
+                  shape: BoxShape.circle,
+                  color: (isStatus ?? false) ? Colors.green : Colors.grey,
+                ),
+              ),
+            ),
+        ],
+      ),
+    );
+  }
+}
