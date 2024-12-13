d0eecb76609f4606e2dae47f64ea4aa4ac63aa46
horiyu <horiyu.dev@gmail.com>
2024-12-13 15:22:32 +0900

    SignedInShellウィジェットを追加し、サインイン状態に応じた画面遷移を実装

---
+++ lib/presentation/router/signed_in_shell.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+import 'package:my_web_app/presentation/pages/signin_page.dart';
+import 'package:my_web_app/presentation/widgets/splash.dart';
+
+// import '../../logic/credential/types/credential.dart';
+// import '../../state/credential/provider.dart';
+// import '../pages/sign_in.dart';
+// import '../widgets/splash.dart';
+
+/// サインイン限定の画面範囲
+class SignedInShell extends ConsumerWidget {
+  const SignedInShell({
+    super.key,
+    required this.builder,
+  });
+
+  final Widget Function(Credential credential) builder;
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final asyncCredential = ref.watch(credentialProvider);
+
+    switch (asyncCredential) {
+      case AsyncData(:final value):
+        if (value == null) {
+          return const SignInPage();
+        } else {
+          return builder(value);
+        }
+      default:
+        return const Splash(isLoading: true);
+    }
+  }
+}
