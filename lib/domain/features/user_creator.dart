f1fd6eeadd08b5dedbca9f27e21c4104aa28f4f2
horiyu <horiyu.dev@gmail.com>
2024-12-15 05:22:15 +0900

    ユーザー作成機能にメールフィールドを追加し、新規ユーザー情報の整合性を向上

--- lib/domain/features/user_creator.dart
+++ lib/domain/features/user_creator.dart
@@ -1,18 +1,23 @@
+import 'dart:math';
+
 import '../types/user.dart';
 
 class UserCreater {
   UserCreater({
     required this.uid,
     required this.name,
+    required this.email,
   });
 
   final String uid;
   final String name;
+  final String email;
 
   User createNewUser() {
     return User(
       uid: uid,
       name: name,
+      email: email,
       handle: uid,
       isHima: false,
       deadline: DateTime.now(),
