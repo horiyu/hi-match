ca860e582a5a42573036d15343f5cd09cbfa6cf0
horiyu <horiyu.dev@gmail.com>
2024-12-15 16:53:47 +0900

    フレーバーを読み込む機能を追加し、環境に応じたフレーバーを設定するように修正

---
+++ lib/application/config/flavor.dart
@@ -0,0 +1,16 @@
+import '../../domain/types/flavor.dart';
+
+final flavor = _readFlavor();
+
+Flavor _readFlavor() {
+  const flavorString = String.fromEnvironment('flavor');
+  if (flavorString == 'dev') {
+    return Flavor.dev;
+  } else if (flavorString == 'stg') {
+    return Flavor.stg;
+  } else if (flavorString == 'prd') {
+    return Flavor.prd;
+  } else {
+    return Flavor.dev;
+  }
+}
