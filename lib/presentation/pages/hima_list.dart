0489140d0831146d43ea8e903f82dbb73e676341
horiyu <horiyu.dev@gmail.com>
2024-12-20 16:59:46 +0900

    HimaListPageをConsumerWidgetに変更し、ユーザーリストの非同期取得を実装

--- lib/presentation/pages/hima_list.dart
+++ lib/presentation/pages/hima_list.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:my_web_app/application/state/hima_list/provider.dart';
 import 'package:my_web_app/infrastructure/firestore/interface.dart';
 import 'package:my_web_app/presentation/theme/colors.dart';
 import 'package:my_web_app/presentation/widgets/tag.dart';
@@ -7,45 +9,22 @@ import 'package:my_web_app/presentation/widgets/user_icon.dart';
 
 import '../../domain/features/hima_checker.dart';
 import '../../domain/types/user.dart';
+import '../../infrastructure/firestore/provider.dart';
 import '../widgets/bottom_navi.dart';
 import '../widgets/count_down_widget.dart';
 
-class HimaListPage extends StatefulWidget {
-  final Firestore firestore;
+class HimaListPage extends ConsumerWidget {
+  HimaListPage({super.key});
 
-  const HimaListPage({super.key, required this.firestore});
+// class _HimaListPageState extends State<HimaListPage> {
+  final bool _isMeHima = false;
 
-  @override
-  State<HimaListPage> createState() => _HimaListPageState();
-}
-
-class _HimaListPageState extends State<HimaListPage> {
-  bool _isMeHima = false;
-
-  List<User> users = [];
+  // List<User> users = [];
 
   @override
-  void initState() {
-    super.initState();
-    _getIsMeHima();
-    _loadUsers();
-  }
+  Widget build(BuildContext context, WidgetRef ref) {
+    final usersAsyncValue = ref.watch(himaListProvider);
 
-  Future<void> _getIsMeHima() async {
-    final me =
-        await widget.firestore.findUserByUid('S5EcL2tMsWcMWK6cNV0ugFYaqpB2');
-    _isMeHima =
-        HimaChecker(isHima: me.isHima, deadline: me.deadline).checkHima();
-    setState(() {});
-  }
-
-  Future<void> _loadUsers() async {
-    users = await widget.firestore.getUsers();
-    setState(() {});
-  }
-
-  @override
-  Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
           leading: Padding(
@@ -56,68 +35,74 @@ class _HimaListPageState extends State<HimaListPage> {
           isStatus: _isMeHima,
         ),
       )),
-      body: ListView.builder(
-        itemExtent: 70,
-        itemCount: users.length,
-        itemBuilder: (context, index) {
-          users.sort((a, b) {
-            if (HimaChecker(isHima: a.isHima, deadline: a.deadline)
-                    .checkHima() &&
-                !HimaChecker(isHima: b.isHima, deadline: b.deadline)
-                    .checkHima()) {
-              return -1;
-            }
-            if (!HimaChecker(isHima: a.isHima, deadline: a.deadline)
-                    .checkHima() &&
-                HimaChecker(isHima: b.isHima, deadline: b.deadline)
-                    .checkHima()) {
-              return 1;
-            }
-            if (a.uid == 'S5EcL2tMsWcMWK6cNV0ugFYaqpB2') return -1;
-            if (b.uid == 'S5EcL2tMsWcMWK6cNV0ugFYaqpB2') return 1;
-            return b.deadline.compareTo(a.deadline);
-          });
+      body: usersAsyncValue.when(
+        data: (users) => ListView.builder(
+          itemExtent: 70,
+          itemCount: users.length,
+          itemBuilder: (context, index) {
+            users.sort((a, b) {
+              if (HimaChecker(isHima: a.isHima, deadline: a.deadline)
+                      .checkHima() &&
+                  !HimaChecker(isHima: b.isHima, deadline: b.deadline)
+                      .checkHima()) {
+                return -1;
+              }
+              if (!HimaChecker(isHima: a.isHima, deadline: a.deadline)
+                      .checkHima() &&
+                  HimaChecker(isHima: b.isHima, deadline: b.deadline)
+                      .checkHima()) {
+                return 1;
+              }
+              if (a.uid == 'S5EcL2tMsWcMWK6cNV0ugFYaqpB2') return -1;
+              if (b.uid == 'S5EcL2tMsWcMWK6cNV0ugFYaqpB2') return 1;
+              return b.deadline.compareTo(a.deadline);
+            });
 
-          final user = users[index];
-          bool isMe = user.uid == 'S5EcL2tMsWcMWK6cNV0ugFYaqpB2';
-          bool isCheckedHima = HimaChecker(
-            isHima: user.isHima,
-            deadline: user.deadline,
-          ).checkHima();
+            final user = users[index];
+            bool isMe = user.uid == 'S5EcL2tMsWcMWK6cNV0ugFYaqpB2';
+            bool isCheckedHima = HimaChecker(
+              isHima: user.isHima,
+              deadline: user.deadline,
+            ).checkHima();
 
-          return ListTile(
-            leading: Opacity(
-              opacity: isCheckedHima ? 1 : 0.5,
-              child: UserIcon(
-                size: 50,
-                // imageUrl: user.avatar,
-                isDisplayedStatus: isCheckedHima,
-                isStatus: isCheckedHima,
+            return ListTile(
+              leading: Opacity(
+                opacity: isCheckedHima ? 1 : 0.5,
+                child: UserIcon(
+                  size: 50,
+                  // imageUrl: user.avatar,
+                  isDisplayedStatus: isCheckedHima,
+                  isStatus: isCheckedHima,
+                ),
               ),
-            ),
-            title: Text(
-              user.name,
-              style: TextStyle(
-                fontWeight: FontWeight.bold,
-                color: isCheckedHima
-                    ? BrandColors.black
-                    : BrandColors.black.withOpacity(0.5),
+              title: Text(
+                user.name,
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  color: isCheckedHima
+                      ? BrandColors.black
+                      : BrandColors.black.withOpacity(0.5),
+                ),
               ),
-            ),
-            subtitle: isCheckedHima
-                ? Row(
-                    children: [
-                      TagWidget(text: user.name, height: 25),
-                    ],
-                  )
-                : null,
-            trailing:
-                isCheckedHima ? CountdownWidget(deadline: user.deadline) : null,
-            onTap: () {},
-            tileColor:
-                isMe ? BrandColors.primary.withOpacity(0.2) : BrandColors.white,
-          );
-        },
+              subtitle: isCheckedHima
+                  ? Row(
+                      children: [
+                        TagWidget(text: user.name, height: 25),
+                      ],
+                    )
+                  : null,
+              trailing: isCheckedHima
+                  ? CountdownWidget(deadline: user.deadline)
+                  : null,
+              onTap: () {},
+              tileColor: isMe
+                  ? BrandColors.primary.withOpacity(0.2)
+                  : BrandColors.white,
+            );
+          },
+        ),
+        loading: () => Center(child: CircularProgressIndicator()),
+        error: (error, stack) => Center(child: Text('Error: $error')),
       ),
       // bottomNavigationBar: const BottomNavigationBarWidget(),
     );
