4f9e3fbff533ac5b11847e77d88130bfc4eeb8f6
horiyu <horiyu.dev@gmail.com>
2024-12-21 02:53:31 +0900

    BottomNavigationBarWidgetのコードをコメントアウトし、不要な依存関係を削除

--- lib/presentation/widgets/bottom_navi.dart
+++ lib/presentation/widgets/bottom_navi.dart
@@ -1,75 +1,58 @@
-import 'package:flutter/material.dart';
-import 'package:my_web_app/presentation/theme/colors.dart';
+// import 'package:flutter/material.dart';
+// import '../pages/hima_list.dart';
 
-import '../../infrastructure/firestore/impl_dev.dart';
-import '../pages/hima_list.dart';
+// class BottomNavigationBarWidget extends StatefulWidget {
+//   @override
+//   _BottomNavigationBarWidgetState createState() =>
+//       _BottomNavigationBarWidgetState();
+// }
 
-class BottomNavigationBarWidget extends StatefulWidget {
-  const BottomNavigationBarWidget({super.key});
+// class _BottomNavigationBarWidgetState extends State<BottomNavigationBarWidget> {
+//   int _selectedIndex = 0;
 
-  @override
-  _BottomNavigationBarWidgetState createState() =>
-      _BottomNavigationBarWidgetState();
-}
+//   static final List<Widget> _widgetOptions = <Widget>[
+//     Text(
+//       'Index 1: Business',
+//     ),
+//     Text(
+//       'Index 2: School',
+//     ),
+//     Text(
+//       'Index 3: School',
+//     ),
+//   ];
 
-class _BottomNavigationBarWidgetState extends State<BottomNavigationBarWidget> {
-  int _selectedIndex = 0;
+//   void _onItemTapped(int index) {
+//     setState(() {
+//       _selectedIndex = index;
+//     });
+//   }
 
-  static const List<Widget> _widgetOptions = <Widget>[
-    Text('Home Page'),
-    Text('Search Page'),
-    Text('Add Page'),
-    Text('Profile Page'),
-  ];
-
-  void _onItemTapped(int index) {
-    setState(() {
-      _selectedIndex = index;
-    });
-
-    if (index == 0) {
-      Navigator.push(
-        context,
-        MaterialPageRoute(
-          builder: (context) => HimaListPage(),
-        ),
-      );
-    }
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: _widgetOptions.elementAt(_selectedIndex),
-      ),
-      bottomNavigationBar: BottomNavigationBar(
-        items: const <BottomNavigationBarItem>[
-          BottomNavigationBarItem(
-            icon: Icon(Icons.home),
-            label: 'Home',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.search),
-            label: 'Search',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.add_box),
-            label: 'Add',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.person),
-            label: 'Profile',
-          ),
-        ],
-        currentIndex: _selectedIndex,
-        selectedItemColor: BrandColors.primary,
-        unselectedItemColor: BrandColors.black,
-        onTap: _onItemTapped,
-        showSelectedLabels: false,
-        showUnselectedLabels: false,
-        type: BottomNavigationBarType.fixed,
-      ),
-    );
-  }
-}
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       body: Center(
+//         child: _widgetOptions.elementAt(_selectedIndex),
+//       ),
+//       bottomNavigationBar: BottomNavigationBar(
+//         items: const <BottomNavigationBarItem>[
+//           BottomNavigationBarItem(
+//             icon: Icon(Icons.home),
+//             label: 'Home',
+//           ),
+//           BottomNavigationBarItem(
+//             icon: Icon(Icons.business),
+//             label: 'Business',
+//           ),
+//           BottomNavigationBarItem(
+//             icon: Icon(Icons.school),
+//             label: 'School',
+//           ),
+//         ],
+//         currentIndex: _selectedIndex,
+//         selectedItemColor: Colors.amber[800],
+//         onTap: _onItemTapped,
+//       ),
+//     );
+//   }
+// }
