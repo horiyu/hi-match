ff0f07426c9a5c6f061296989a5ca64a80c29720
horiyu <horiyu.dev@gmail.com>
2024-12-20 17:00:00 +0900

    MaterialAppの設定を更新し、GoRouterを使用したルーティングを実装: コンソールログとローカライズを追加

--- lib/presentation/router/app.dart
+++ lib/presentation/router/app.dart
@@ -1,8 +1,20 @@
 import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:intl/intl.dart';
 import 'package:my_web_app/firebase/analytics_repository.dart';
+import 'package:my_web_app/infrastructure/firestore/impl_dev.dart';
 import 'package:my_web_app/presentation/pages/hima_list.dart';
 import 'package:my_web_app/presentation/theme/fonts.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+
+import '../../application/state/flavor/provider.dart';
+import '../../infrastructure/console/provider.dart';
+import 'go_router.dart';
+import 'mobile_simulator_view.dart';
+
+void main() {
+  runApp(const ProviderScope(child: App()));
+}
 
 class App extends ConsumerWidget {
   const App({super.key});
@@ -12,16 +24,39 @@ class App extends ConsumerWidget {
     var analytics = ref.watch(analyticsRepository);
     var analyticsObserver = ref.watch(analyticsObserverRepository);
 
-    analytics.logAppOpen();
+    final console = ref.watch(consoleProvider);
+    final router = ref.watch(goRouterProvider);
 
-    return MaterialApp(
-      title: 'ひマッチ',
+    analytics.logAppOpen();
+    console.yellow('App.build() called FLAVOR: ${flavor.name}');
+
+    return MaterialApp.router(
+      routerDelegate: router.routerDelegate, // GoRouter
+      routeInformationParser: router.routeInformationParser, // GoRouter
+      routeInformationProvider: router.routeInformationProvider, // GoRouter
+      debugShowCheckedModeBanner: false,
+      builder: (_, child) {
+        return MobileSimulatorView(
+          child: HimaListPage() ?? const SizedBox.shrink(),
+        );
+      },
+      localizationsDelegates:
+          AppLocalizations.localizationsDelegates, // localizations
+      supportedLocales: AppLocalizations.supportedLocales, // localizations
+      locale: Locale(Intl.systemLocale),
       theme: ThemeData(
-        fontFamily: BrandText.bodyS.fontFamily,
+        fontFamily: BrandFont.general,
       ),
-      initialRoute: '/',
-      navigatorObservers: [analyticsObserver],
-      home: const HimaListPage(), // ここにホームページウィジェットを指定
     );
+
+    // return MaterialApp(
+    //   title: 'ひマッチ',
+    //   theme: ThemeData(
+    //     fontFamily: BrandText.bodyS.fontFamily,
+    //   ),
+    //   initialRoute: '/',
+    //   navigatorObservers: [analyticsObserver],
+    //   home: HimaListPage(firestore: ImplDev()), // ここでfirestoreインスタンスを渡す
+    // );
   }
 }
