7c7d64555a00fa5903cd727b2aea0c05b35e3560
horiyu <horiyu.dev@gmail.com>
2024-12-21 22:22:53 +0900

    PlanPageウィジェットを追加し、基本的なレイアウトを実装

---
+++ lib/presentation/pages/plan.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class PlanPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: const Center(
+        child: Text('This is the Plan Page'),
+      ),
+    );
+  }
+}
