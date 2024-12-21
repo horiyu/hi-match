827e3f0dde0a05637841937d0223140ec9311597
horiyu <horiyu.dev@gmail.com>
2024-12-21 22:08:40 +0900

    GoRouterにViewPageを追加し、初期位置をViewPageに変更

--- lib/presentation/router/go_router.dart
+++ lib/presentation/router/go_router.dart
@@ -1,15 +1,23 @@
+import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
 import 'package:my_web_app/presentation/pages/hima_list.dart';
+import 'package:my_web_app/presentation/pages/profile.dart';
 import 'package:my_web_app/presentation/pages/sign_in.dart';
 import 'package:my_web_app/presentation/pages/sign_up.dart';
 import 'package:my_web_app/presentation/pages/hima_list.dart';
+import '../../application/state/me/provider.dart';
+import '../../application/state/user/provider.dart';
+import '../../infrastructure/auth/provider.dart';
+import 'pageView.dart';
 import 'page_path.dart';
 import 'signed_in_shell.dart';
 import 'splash_completed_shell.dart';
 
 final goRouterProvider = Provider(
   (ref) {
+    final auth = ref.watch(authProvider);
+
     // サインイン画面
     final signIn = GoRoute(
       path: PagePath.signIn.path,
@@ -17,10 +25,18 @@ final goRouterProvider = Provider(
       builder: (_, __) => const SignInPage(),
     );
 
+    // サインアップ画面
     final signUp = GoRoute(
       path: PagePath.signUp.path,
       name: PagePath.signUp.name,
       builder: (_, __) => const SignUpPage(),
+    );
+
+    // ViewPage
+    final viewPage = GoRoute(
+      path: PagePath.viewPage.path,
+      name: PagePath.viewPage.name,
+      builder: (_, __) => ViewPage(),
     );
 
     // // ホーム画面
@@ -30,20 +46,46 @@ final goRouterProvider = Provider(
     //   builder: (_, __) => const HimaListPage(),
     // );
 
-    final himaList = GoRoute(
-      path: PagePath.himaList.path,
-      name: PagePath.himaList.name,
-      builder: (_, __) => HimaListPage(),
-    );
+    // final himaList = GoRoute(
+    //   path: PagePath.himaList.path,
+    //   name: PagePath.himaList.name,
+    //   builder: (_, __) => HimaListPage(),
+    // );
+
+    // final meAsyncValue = ref.watch(meProvider);
+
+    // final profile = GoRoute(
+    //   path: PagePath.profile.path,
+    //   name: PagePath.profile.name,
+    //   builder: (_, __) {
+    //     return meAsyncValue.when(
+    //       data: (user) => ProfilePage(user: user),
+    //       loading: () => const CircularProgressIndicator(),
+    //       error: (err, stack) => Text('Error: $err'),
+    //     );
+    //   },
+    // );
 
     // サインインしないと見れない画面範囲
     final signedInShell = ShellRoute(
       routes: [
-        himaList,
-        GoRoute(
-          path: '/home',
-          builder: (_, __) => HimaListPage(),
-        ),
+        // himaList,
+        // profile,
+        viewPage,
+        // GoRoute(
+        //   path: '/home',
+        //   builder: (_, __) => HimaListPage(),
+        // ),
+        // GoRoute(
+        //   path: '/profile',
+        //   builder: (_, __) {
+        //     return meAsyncValue.when(
+        //       data: (user) => ProfilePage(user: user),
+        //       loading: () => const CircularProgressIndicator(),
+        //       error: (err, stack) => Text('Error: $err'),
+        //     );
+        //   },
+        // ),
         // GoRoute(
         //   path: '/search',
         //   builder: (_, __) => SearchPage(),
@@ -79,11 +121,22 @@ final goRouterProvider = Provider(
     );
 
     return GoRouter(
-      initialLocation: PagePath.himaList.path,
+      // initialLocation: PagePath.himaList.path,
+      initialLocation: PagePath.viewPage.path,
       debugLogDiagnostics: false,
       routes: [
         splashCompletedShell,
       ],
+      // redirect: (context, state) {
+      //   final loggedIn = ref.read(authProvider).isSignedIn as bool;
+      //   final goingToSignIn = state.uri.toString() == PagePath.signIn.path;
+      //   print('loggedIn: $loggedIn');
+      //   print('goingToSignIn: $goingToSignIn');
+      //   if (!loggedIn && !goingToSignIn) {
+      //     return PagePath.signIn.path;
+      //   }
+      //   return null;
+      // }
     );
   },
 );
