e97bdf80f8224804a126b150ee34b61291ff6405
horiyu <horiyu.dev@gmail.com>
2024-12-21 17:19:22 +0900

    ProfilePageを追加し、ユーザー情報を表示するウィジェットを実装

---
+++ lib/presentation/pages/profile.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+import 'package:my_web_app/domain/types/user.dart';
+
+import '../widgets/user_icon.dart';
+
+class ProfilePage extends StatelessWidget {
+  final User user;
+
+  const ProfilePage({super.key, required this.user});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Profile'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            UserIcon(
+              size: 100,
+              isDisplayedStatus: true,
+              isStatus: true,
+              onTap: () {},
+            ),
+            const SizedBox(height: 20),
+            const Text(
+              'Your Name',
+              style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+            ),
+            const SizedBox(height: 10),
+            const Text(
+              'your.email@example.com',
+              style: TextStyle(fontSize: 16, color: Colors.grey),
+            ),
+            const SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: () {
+                // Add your logout logic here
+              },
+              child: const Text('Logout'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
