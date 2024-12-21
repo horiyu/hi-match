fca337a50ac9b17cc7206e1c123d3532ae78fd46
horiyu <horiyu.dev@gmail.com>
2024-12-22 01:47:32 +0900

    HimaModalを追加し、ユーザーIDに基づくひま時間の選択とひまつぶし設定機能を実装

---
+++ lib/presentation/pages/hima_modal.dart
@@ -0,0 +1,108 @@
+import 'package:flutter/material.dart';
+import '../../components/custom_time_picker.dart';
+import '../../components/hima_activity_list.dart';
+import '../widgets/pill_outlined_button.dart';
+
+class HimaModal extends StatefulWidget {
+  final String uid;
+
+  const HimaModal(this.uid, {super.key});
+
+  @override
+  _HimaModalState createState() => _HimaModalState();
+}
+
+class _HimaModalState extends State<HimaModal> {
+  DateTime? selectedDate;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Hima Modal'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            PillOutlinedButton(
+              onPressed: () async {
+                await showCustomTimePicker(
+                  sheetHeight: 500,
+                  itemHeight: 35,
+                  textSize: 20,
+                  selectedTime: selectedDate,
+                  handler: ({required DateTime date}) {
+                    setState(() {
+                      selectedDate = date;
+                    });
+                  },
+                  context: context,
+                );
+              },
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  const Text('ひま時間'),
+                  Row(
+                    children: [
+                      if (selectedDate == null)
+                        const Text('時間を選択')
+                      else
+                        Text(
+                          '〜 ${selectedDate!.hour.toString().padLeft(2, '0')}:${selectedDate!.minute.toString().padLeft(2, '0')}',
+                        ),
+                      const SizedBox(width: 10),
+                      const Icon(Icons.chevron_right),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+            const SizedBox(height: 16.0),
+            PillOutlinedButton(
+              onPressed: () async {
+                await himaActivityList(
+                  handler: ({required String himaActivities}) {
+                    setState(() {
+                      // Handle himaActivities
+                    });
+                  },
+                  context: context,
+                  uid: widget.uid,
+                );
+              },
+              child: const Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text('ひまつぶしを設定する'),
+                  Icon(Icons.chevron_right),
+                ],
+              ),
+            ),
+            const Spacer(),
+            Padding(
+              padding: const EdgeInsets.only(bottom: 30),
+              child: ElevatedButton(
+                style: ButtonStyle(
+                  backgroundColor: WidgetStateProperty.all(
+                    selectedDate == null
+                        ? Colors.deepOrangeAccent.withOpacity(0.5)
+                        : Colors.deepOrangeAccent,
+                  ),
+                  minimumSize: WidgetStateProperty.all(const Size(300, 50)),
+                ),
+                onPressed: selectedDate == null ? null : () async {},
+                child: const Text(
+                  'ひま',
+                  style: TextStyle(color: Colors.white),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
