be0b1b6d4bef00a7bac93c6d56f1eff876f2568f
horiyu <horiyu.dev@gmail.com>
2024-12-31 01:57:30 +0900

    UIの改善: HimaModalをStatefulWidgetからStatelessWidgetに変更し、コードを簡素化

--- lib/presentation/pages/hima_modal.dart
+++ lib/presentation/pages/hima_modal.dart
@@ -3,105 +3,98 @@ import '../../components/custom_time_picker.dart';
 import '../../components/hima_activity_list.dart';
 import '../widgets/pill_outlined_button.dart';
 
-class HimaModal extends StatefulWidget {
+class HimaModal extends StatelessWidget {
   final String uid;
 
   const HimaModal(this.uid, {super.key});
 
   @override
-  _HimaModalState createState() => _HimaModalState();
-}
-
-class _HimaModalState extends State<HimaModal> {
-  DateTime? selectedDate;
-
-  @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Hima Modal'),
+    DateTime? selectedDate;
+
+    return Container(
+      height: MediaQuery.of(context).size.height * 0.9,
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.vertical(top: Radius.circular(20)),
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          mainAxisSize: MainAxisSize.min,
-          children: [
-            PillOutlinedButton(
-              onPressed: () async {
-                await showCustomTimePicker(
-                  sheetHeight: 500,
-                  itemHeight: 35,
-                  textSize: 20,
-                  selectedTime: selectedDate,
-                  handler: ({required DateTime date}) {
-                    setState(() {
-                      selectedDate = date;
-                    });
-                  },
-                  context: context,
-                );
-              },
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  const Text('ひま時間'),
-                  Row(
-                    children: [
-                      if (selectedDate == null)
-                        const Text('時間を選択')
-                      else
-                        Text(
-                          '〜 ${selectedDate!.hour.toString().padLeft(2, '0')}:${selectedDate!.minute.toString().padLeft(2, '0')}',
-                        ),
-                      const SizedBox(width: 10),
-                      const Icon(Icons.chevron_right),
-                    ],
-                  ),
-                ],
-              ),
-            ),
-            const SizedBox(height: 16.0),
-            PillOutlinedButton(
-              onPressed: () async {
-                await himaActivityList(
-                  handler: ({required String himaActivities}) {
-                    setState(() {
-                      // Handle himaActivities
-                    });
-                  },
-                  context: context,
-                  uid: widget.uid,
-                );
-              },
-              child: const Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Text('ひまつぶしを設定する'),
-                  Icon(Icons.chevron_right),
-                ],
-              ),
-            ),
-            const Spacer(),
-            Padding(
-              padding: const EdgeInsets.only(bottom: 30),
-              child: ElevatedButton(
-                style: ButtonStyle(
-                  backgroundColor: WidgetStateProperty.all(
-                    selectedDate == null
-                        ? Colors.deepOrangeAccent.withOpacity(0.5)
-                        : Colors.deepOrangeAccent,
-                  ),
-                  minimumSize: WidgetStateProperty.all(const Size(300, 50)),
+      padding: const EdgeInsets.all(16.0),
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          PillOutlinedButton(
+            height: 50,
+            onPressed: () async {
+              await showCustomTimePicker(
+                sheetHeight: 500,
+                itemHeight: 35,
+                textSize: 20,
+                selectedTime: selectedDate,
+                handler: ({required DateTime date}) {
+                  selectedDate = date;
+                },
+                context: context,
+              );
+            },
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                const Text('ひま時間'),
+                Row(
+                  children: [
+                    if (selectedDate == null)
+                      const Text('時間を選択')
+                    else
+                      Text(
+                        '〜 ${selectedDate!.hour.toString().padLeft(2, '0')}:${selectedDate!.minute.toString().padLeft(2, '0')}',
+                      ),
+                    const SizedBox(width: 10),
+                    const Icon(Icons.chevron_right),
+                  ],
                 ),
-                onPressed: selectedDate == null ? null : () async {},
-                child: const Text(
-                  'ひま',
-                  style: TextStyle(color: Colors.white),
+              ],
+            ),
+          ),
+          const SizedBox(height: 16.0),
+          PillOutlinedButton(
+            height: 50,
+            onPressed: () async {
+              await himaActivityList(
+                handler: ({required String himaActivities}) {
+                  // Handle himaActivities
+                },
+                context: context,
+                uid: uid,
+              );
+            },
+            child: const Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text('ひまつぶしを設定する'),
+                Icon(Icons.chevron_right),
+              ],
+            ),
+          ),
+          const Spacer(),
+          Padding(
+            padding: const EdgeInsets.only(bottom: 30),
+            child: ElevatedButton(
+              style: ButtonStyle(
+                backgroundColor: MaterialStateProperty.all(
+                  selectedDate == null
+                      ? Colors.deepOrangeAccent.withOpacity(0.5)
+                      : Colors.deepOrangeAccent,
                 ),
+                minimumSize: MaterialStateProperty.all(const Size(300, 50)),
+              ),
+              onPressed: selectedDate == null ? null : () async {},
+              child: const Text(
+                'ひま',
+                style: TextStyle(color: Colors.white),
               ),
             ),
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }
